8966e721965d6c9da40651361cc88ee665f3212c
balmacefa <fabianbalmaceda2@gmail.com>
2020-03-11 11:24:19 -0600

    Reading helper divider

--- src/components/exercises/ReadingHelper/ReadingHelper.js
+++ src/components/exercises/ReadingHelper/ReadingHelper.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import ReactAnimationFrame from 'react-animation-frame';
 import ReadingHelperController from './ReadingHelperController';
+import $ from 'jquery';
 
 class ReadingHelper extends Component {
 
@@ -10,13 +11,16 @@ class ReadingHelper extends Component {
         this.state = {
             init: false,
             wpm: 400,
-            divider: 70,
-            context: props.context,
+            divider: 25
         };
 
         this.wordRef = 0;
 
         this.removeWords = false;
+
+        this.toSpanSelector = '#toSpanSelector';
+        this.disappearAnimationClass = 'disappear-animation';
+        this.appearAnimationClass = 'appear-animation';
 
         this.nextFireWord = 0;
         this.userRemoveWords = this
@@ -25,16 +29,46 @@ class ReadingHelper extends Component {
         this.userChangeWPM = this
             .userChangeWPM
             .bind(this);
+        this.userResetWords = this
+            .userResetWords
+            .bind(this);
+        this.userChangeDividerSpacing = this
+            .userChangeDividerSpacing
+            .bind(this);
+    }
+
+    componentDidMount() {
+        $(this.toSpanSelector)
+            .each(function () {
+                var text = $(this)
+                    .text()
+                    .split(' ');
+
+                for (var i = 0, len = text.length; i < len; i++) {
+                    text[i] = '<span id="word-' + i + '">' + text[i] + '</span>';
+                }
+                $(this).html(text.join(' '));
+
+            });
+
     }
 
     wpmToMs(wpm) {
         return 60000 / wpm;
     }
 
-    tick() {}
+    tick() {
+        //AddClass
+        let _this = this;
+        $(`#word-${_this.wordRef}`).addClass(this.disappearAnimationClass);
+        this.wordRef++;
+    }
 
     onAnimationFrame(time) {
-        if (this.userRemoveWords && time > this.nextFireWord) {
+        if (this.wordRef === 4) {
+            $(`${this.toSpanSelector} .${this.appearAnimationClass}`).removeClass(this.appearAnimationClass);
+        }
+        if (this.removeWords && time > this.nextFireWord) {
             this.nextFireWord = time + this.wpmToMs(this.state.wpm);
 
             this.tick();
@@ -51,14 +85,18 @@ class ReadingHelper extends Component {
     }
 
     userRemoveWords(event) {
-    console.log('sd');
-        this.state.context.hola();
-        this.removeWords = event.target.value;
+        this.removeWords = event.target.checked;
+    }
 
-        if (!this.removeWords) {
-            //clear UI and counter
-            
-        }
+    userResetWords() {
+        $(`${this.toSpanSelector} .${this.disappearAnimationClass}`)
+            .removeClass(this.disappearAnimationClass)
+            .addClass(this.appearAnimationClass);
+        this.wordRef = 0;
+    }
+
+    userChangeDividerSpacing(event) {
+        this.setState({divider: event.target.value});
     }
 
     render() {
@@ -70,7 +108,10 @@ class ReadingHelper extends Component {
                     <ReadingHelperController
                         wpm={this.state.wpm}
                         userChangeWPM={this.userChangeWPM}
-                        userRemoveWords={this.userRemoveWords}/>
+                        userRemoveWords={this.userRemoveWords}
+                        userResetWords={this.userResetWords}
+                        userChangeDividerSpacing={this.userChangeDividerSpacing}
+                        dividerSpacing={this.state.divider}/>
 
                     <div className="row justify-content-center text-justify">
                         <div className="col-md-6 mt-5 mb-5">
@@ -86,32 +127,32 @@ class ReadingHelper extends Component {
                                 right: this.state.divider / 2 + '%'
                             }}></div>
 
-                            <p className="text-lighter">Have you ever wondered what it means to live in the
-                                present? Arent we all here, now, in the present? Technically, yes, but for so
-                                many of us, we are only 10 percent here. We are really living in our minds. We
-                                exist from day-to-day in a dream-like state where were not really connected to
-                                the world around us, nor centered in our own body or being. Instead, were
-                                preoccupied with memories of the past, churning thoughts and worries about the
-                                future, and judgments and reactions to the few things we do see. Were literally
-                                missing out on most our own lives, which leaves us feeling shallow, empty and
-                                deeply unsettled. The great news is you can learn to be more present and
-                                mindful. Here are 10 easy ways you can practice mindfulness in your daily life.
-                                1. Eat mindfully. When you scoff down your meal on autopilot while distracted by
-                                the television, computer or constant conversation, you miss out on the delicious
-                                taste and smell of your food. Youre also less likely to feel satisfied and
-                                nourished, because you missed out on the fact that you ate. It can be helpful to
-                                remember this phrase: When you eat, eat. When you drink, drink. In other words,
-                                dont attempt to do fifty other things when you sit down to a meal, coffee or
-                                green juice. Simply focus all of your attention on what is in front of you. 2.
-                                Walk mindfully. The essential guide to taking care of your mind and body
-                                Subscribe to HuffPost’s wellness email Thanks! You have been successfully signed
-                                up. Take a beautiful tip from spiritual leader Thich Nhat Hanh and walk as if
-                                you are kissing the earth with your feet. In others words, when you are out and
-                                about, pay attention occasionally to the movement of your body and your
-                                surroundings. Notice as your feet connect with and leave the ground. Feel your
-                                muscles moving and supporting you. Observe what is going on around you -- the
-                                sights, sounds and life unfolding. You may be amazed to find a whole new world
-                                you hadnt even noticed before.
+                            <p id="toSpanSelector" className="text-lighter">Have you ever wondered what it
+                                means to live in the present? Arent we all here, now, in the present?
+                                Technically, yes, but for so many of us, we are only 10 percent here. We are
+                                really living in our minds. We exist from day-to-day in a dream-like state where
+                                were not really connected to the world around us, nor centered in our own body
+                                or being. Instead, were preoccupied with memories of the past, churning thoughts
+                                and worries about the future, and judgments and reactions to the few things we
+                                do see. Were literally missing out on most our own lives, which leaves us
+                                feeling shallow, empty and deeply unsettled. The great news is you can learn to
+                                be more present and mindful. Here are 10 easy ways you can practice mindfulness
+                                in your daily life. 1. Eat mindfully. When you scoff down your meal on autopilot
+                                while distracted by the television, computer or constant conversation, you miss
+                                out on the delicious taste and smell of your food. Youre also less likely to
+                                feel satisfied and nourished, because you missed out on the fact that you ate.
+                                It can be helpful to remember this phrase: When you eat, eat. When you drink,
+                                drink. In other words, dont attempt to do fifty other things when you sit down
+                                to a meal, coffee or green juice. Simply focus all of your attention on what is
+                                in front of you. 2. Walk mindfully. The essential guide to taking care of your
+                                mind and body Subscribe to HuffPost’s wellness email Thanks! You have been
+                                successfully signed up. Take a beautiful tip from spiritual leader Thich Nhat
+                                Hanh and walk as if you are kissing the earth with your feet. In others words,
+                                when you are out and about, pay attention occasionally to the movement of your
+                                body and your surroundings. Notice as your feet connect with and leave the
+                                ground. Feel your muscles moving and supporting you. Observe what is going on
+                                around you -- the sights, sounds and life unfolding. You may be amazed to find a
+                                whole new world you hadnt even noticed before.
                             </p>
                         </div>
                     </div>
--- src/components/exercises/ReadingHelper/ReadingHelperController.js
+++ src/components/exercises/ReadingHelper/ReadingHelperController.js
@@ -38,10 +38,39 @@ export default class ReadingHelperController extends Component {
                                 </div>
 
                                 <label className="switch align-self-center">
-                                    <input type="checkbox" className="warning" onChange={this.props.userRemoveWords}/>
+                                    <input
+                                        type="checkbox"
+                                        className="warning"
+                                        onChange={this.props.userRemoveWords}/>
                                     <span className="slider round"></span>
                                 </label>
                             </div>
+                            <div className="d-flex p-7">
+                                <button
+                                    id="btn-reset-words"
+                                    onClick={() => this.props.userResetWords()}
+                                    className="btn btn-secondary"
+                                    type="button">Reset</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="row justify-content-center d-flex">
+
+                        <div className="d-flex align-self-center min-full-width mt-2">
+
+                            <label className="align-self-center">
+                                <input
+                                    type="range"
+                                    className="custom-range"
+                                    id="customRange1"
+                                    value={this.props.dividerSpacing}
+                                    onChange={e => this
+                                    .props
+                                    .userChangeDividerSpacing(e)}/>
+
+                                <h6 className="text-center text-black-50">Spacing</h6>
+                            </label>
                         </div>
 
                     </div>
@@ -52,6 +81,9 @@ export default class ReadingHelperController extends Component {
 }
 ReadingHelperController.propTypes = {
     wpm: PropTypes.number.isRequired,
+    dividerSpacing: PropTypes.number.isRequired,
     userChangeWPM: PropTypes.func.isRequired,
-    userRemoveWords: PropTypes.func.isRequired
+    userRemoveWords: PropTypes.func.isRequired,
+    userResetWords: PropTypes.func.isRequired,
+    userChangeDividerSpacing: PropTypes.func.isRequired
 };
