37fb8b50fae7871d82eb67f0d60cb347145709c4
balmacefa <fabianbalmaceda2@gmail.com>
2020-03-12 23:00:34 -0600

    Updates

---
+++ precache-manifest.9c874b2cd792b443deff5a0670441d93.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b5e608bcd04b26ecfde97ee7b77724",
+    "url": "/reading-trainer/index.html"
+  },
+  {
+    "revision": "4f47ea03b3c73e179141",
+    "url": "/reading-trainer/static/css/main.7a44d633.chunk.css"
+  },
+  {
+    "revision": "e25486a69ef47ee4341f",
+    "url": "/reading-trainer/static/js/2.190d454e.chunk.js"
+  },
+  {
+    "revision": "5d45fd43e43a71da9ca2f4ceb7e037e1",
+    "url": "/reading-trainer/static/js/2.190d454e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f47ea03b3c73e179141",
+    "url": "/reading-trainer/static/js/main.c230db20.chunk.js"
+  },
+  {
+    "revision": "176f30e8fb7739a5a3d4",
+    "url": "/reading-trainer/static/js/runtime-main.2db871d0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reading-trainer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reading-trainer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reading-trainer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reading-trainer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reading-trainer/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/reading-trainer/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/reading-trainer/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/reading-trainer/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/reading-trainer/static/media/ionicons.621bd386.svg"
+  }
+]);
