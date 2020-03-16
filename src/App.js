8889fc9323aafd1ae6b3e92c713021a439c12bb7
balmacefa <fabianbalmaceda2@gmail.com>
2020-03-15 22:54:35 -0600

    Google analitycs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import "./assets/bootstrap/css/bootstrap.min.css";
@@ -10,6 +10,9 @@ import "./assets/css/Navigation-Clean.css";
 import "./assets/css/styles.css";
 
 import {HashRouter as Router, Switch, Route} from "react-router-dom";
+import {createBrowserHistory} from 'history';
+import ReactGA from 'react-ga';
+import Config from './Config';
 import Header from './components/globals/Header';
 import Footer from './components/globals/Footer';
 import Features from './components/exercises/Features';
@@ -19,38 +22,50 @@ import BlockOfWords from './components/exercises/BlockOfWords/BlockOfWords';
 import SpeedReadingTest from './components/exercises/SpeedReadingTest/SpeedReadingTest';
 import ReadingHelper from './components/exercises/ReadingHelper/ReadingHelper';
 
-function App() {
-    return (
-        <Router>
+export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.history = createBrowserHistory();
+        ReactGA.initialize(Config.googleTrackingId);
+        // Initialize google analytics page view tracking
+        this.history.listen(location => {
+            ReactGA.set({page: location.pathname}); // Update the user's current page
+            ReactGA.pageview(location.pathname); // Record a pageview for the given page
+        });
 
-            <Header/>
-            <Switch>
-                <Route exact path="/">
-                    <Features/>
-                </Route>
+    }
+    render() {
+        return (
+            <Router history={this.history}>
+                <Header/>
+                <Switch>
+                    <Route exact path="/">
+                        <Features/>
+                    </Route>
 
-                <Route path="/speed-reading-test">
-                    <SpeedReadingTest/>
-                </Route>
+                    <Route path="/speed-reading-test">
+                        <SpeedReadingTest/>
+                    </Route>
 
-                <Route path="/running-words">
-                    <RunningWords/>
-                </Route>
-                <Route path="/columns-of-words">
-                    <ColumnsOfWords/>
-                </Route>
+                    <Route path="/running-words">
+                        <RunningWords/>
+                    </Route>
+                    <Route path="/columns-of-words">
+                        <ColumnsOfWords/>
+                    </Route>
 
-                <Route path="/block-of-words">
-                    <BlockOfWords/>
-                </Route>
-                <Route path="/reading-helper">
-                    <ReadingHelper/>
-                </Route>
+                    <Route path="/block-of-words">
+                        <BlockOfWords/>
+                    </Route>
+                    <Route path="/reading-helper">
+                        <ReadingHelper/>
+                    </Route>
 
-            </Switch>
-            <Footer/>
-        </Router>
-    );
+                </Switch>
+                <Footer/>
+            </Router>
+        )
+    }
 }
 
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,5 @@
+const Config = {
+    googleTrackingId: "UA-147989176-3"
+}
+
+export default Config;
